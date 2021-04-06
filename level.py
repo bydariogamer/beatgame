f3c4223a94d8e381e0b77bfe60b134150faa4359
bydariogamer <62303655+bydariogamer@users.noreply.github.com>
2021-04-06 13:47:44 +0200

    update main

--- level.py
+++ level.py
@@ -1,5 +1,5 @@
 import pygame
-
+import numpy
 
 pygame.init()
 pygame.mixer.init()
@@ -8,12 +8,18 @@ pygame.mixer.init()
 class Level:
     def __init__(self, song: pygame.mixer.Sound):
         self.song = song
-        self.duration = int(self.song.get_length())
-        self.array = pygame.sndarray.array(song)
-        self.blocks = []
 
     def gen(self):
-        sampler = len(self.array) // (self.duration * 2)   # the 2 might become a 4 or 3 if needed
-        for foo in range(self.duration * 2):
+        self.duration = int(self.song.get_length())
+        self.array = pygame.sndarray.array(self.song)
+        self.blocks = []
+        sampler = len(self.array) // (self.duration * 4)   # the 2 might become a 4 or 3 if needed
+        for foo in range(int(self.duration * 4) - 1):
             self.blocks.append(self.array[foo*sampler:(foo+1)*sampler-1].mean())
+        min = numpy.min(self.blocks)
+        for block in self.blocks:
+            block -= min
+        max = numpy.max(self.blocks)
+        for block in self.blocks:
+            block %= (max/7)
 
