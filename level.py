ef622f61be98f8f1102dd03b30e2b94db918987f
bydariogamer <62303655+bydariogamer@users.noreply.github.com>
2021-04-06 13:56:51 +0200

    update main

--- level.py
+++ level.py
@@ -3,6 +3,7 @@ import numpy
 
 pygame.init()
 pygame.mixer.init()
+TEST = True
 
 
 class Level:
@@ -16,11 +17,16 @@ class Level:
         sampler = len(self.array) // (self.duration * 4)   # the 2 might become a 4 or 3 if needed
         for foo in range(int(self.duration * 4) - 1):
             self.blocks.append(self.array[foo*sampler:(foo+1)*sampler-1].mean())
+        if TEST:
+            print(self.blocks)
         minimum = min(self.blocks)
         for block in self.blocks:
             block -= minimum
             block = int(block)
+        if TEST:
+            print(self.blocks)
         maximum = max(self.blocks)
         for block in self.blocks:
             block %= (maximum/7)
-
+        if TEST:
+            print(self.blocks)
