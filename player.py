be131ae6e7283a862797b8033c39d1ebcd72f322
bydariogamer <62303655+bydariogamer@users.noreply.github.com>
2021-04-14 09:28:32 +0200

    update player and added particles

--- player.py
+++ player.py
@@ -17,6 +17,7 @@ class Player:
             'dead': pygame.image.load('assets/images/dead.png').convert(),
             'collide': pygame.image.load('assets/images/collide.png').convert()
         }
+        self.particle = pygame.image.load('assets/images/particle.png').convert()
 
         self.rect = self.images['stand'].get_rect()
         self.rect.x = 50
@@ -39,13 +40,6 @@ class Player:
         self.ended = False
 
         self.particles = []
-        self.particle = pygame.Surface((2, 6))
-        self.particle.set_at((0, 1), (255, 0, 0))
-        self.particle.set_at((0, 2), (200, 0, 0))
-        self.particle.set_at((0, 3), (0, 255, 0))
-        self.particle.set_at((0, 4), (0, 200, 0))
-        self.particle.set_at((0, 5), (0, 0, 255))
-        self.particle.set_at((0, 6), (0, 0, 200))
 
     def update(self):
         if self.life:
@@ -77,6 +71,9 @@ class Player:
 
             if self.level.obstacles:
                 self.score += self.combo
+            self.particles.append(self.rect.y)
+            if len(self.particles) > self.combo:
+                self.particles = self.particles[-1:-self.combo]
 
         if self.life < 0:
             self.life = 0
@@ -103,6 +100,8 @@ class Player:
     def draw(self, game):
         game.fill((0, 0, 20))
         pygame.draw.rect(game, (255, 240, 240), (0, 400, 800, 100))
+        for distance, pos_y in enumerate(self.particles):
+            game.blit(self.particle, (self.rect.x - 1 - distance, pos_y))
         for index in range(len(self.level.obstacles)):
             if self.level.obstacles[index].x < 801:
                 pygame.draw.rect(game, self.level.colors[index], self.level.obstacles[index])
