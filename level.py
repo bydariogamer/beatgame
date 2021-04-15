bd9388e8f2f463e92fc1ca885e90e0dd1d419ec1
Sogolumbo <33571916+Sogolumbo@users.noreply.github.com>
2021-04-15 06:18:30 +0200

    Add bpm finder (currently > 80% detection rate)

--- level.py
+++ level.py
@@ -1,3 +1,5 @@
+import numpy as np
+from matplotlib import pyplot as plt
 import pygame
 from colors import neon
 import random
@@ -6,13 +8,51 @@ import random
 pygame.init()
 pygame.mixer.init()
 
-
 class Level:
     def __init__(self, song: pygame.mixer.Sound):
         self.song = song
         self.duration = int(self.song.get_length())
         self.array = pygame.sndarray.array(self.song)
         self.blocks = []
+        
+        stride = 500 # Factor for subsampling
+        minimumBeatsPerSecond = 0.8
+        maximumBeatsPerSecond = 4
+
+        ## Simplify signal
+        mono_signal= np.mean(self.array, 1)
+        # subsample
+        len_subsampled = len(mono_signal) // stride
+        subsampled = np.zeros(len_subsampled)
+        for i in range(len_subsampled):
+            subsampled[i] = np.mean(np.abs(mono_signal[i*stride:(i+1)*stride]))
+        
+        ## Find Tempo / Autocorrelation
+        def default_autocorrelation(length, maximum):
+            result = np.zeros(length)
+            for i in range(length//2 +1):
+                temp = maximum*(i/length)
+                result[i] = temp
+                result[length-i-1] = temp
+            return result
+        def gaussian(x, mu, sigma):
+            return (np.exp(-(x-mu)**2/2/sigma**2)/np.sqrt(2*np.pi)/sigma)
+        autocorrelation = np.correlate(subsampled, subsampled, 'same')
+        corrected_autocorr = autocorrelation - default_autocorrelation(len_subsampled, max(autocorrelation))
+        firstIndex = len_subsampled//2 + int(1/maximumBeatsPerSecond/self.duration*len_subsampled)
+        lastIndex = len_subsampled//2 + int(1/minimumBeatsPerSecond/self.duration*len_subsampled)
+        interestingPart = corrected_autocorr[firstIndex:lastIndex]
+        max_index_intPart = np.argmax(interestingPart)
+        indexBeatLength = max_index_intPart + int(1/maximumBeatsPerSecond/self.duration*len_subsampled)
+        BeatLength = indexBeatLength/len_subsampled * self.duration
+        BeatsPerMinute = 60/BeatLength
+        print('BPM: ', 60/BeatLength)
+        plt.plot(interestingPart)
+        plt.scatter(max_index_intPart, interestingPart[max_index_intPart])
+        plt.show()
+
+
+
         blocks_per_sec = 6
         sampler = len(self.array) // (self.duration * blocks_per_sec)
         for foo in range(int(self.duration * blocks_per_sec) - 1):
