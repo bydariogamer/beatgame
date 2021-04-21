cab40d014186f99cbd68595266da66726b5fac15
bydariogamer <62303655+bydariogamer@users.noreply.github.com>
2021-04-21 07:31:11 +0200

    added colors
    added colors and minor style changes

--- player.py
+++ player.py
@@ -85,14 +85,21 @@ class Player:
                     if self.vel_y < 0:
                         self.vel_y = 0
                     self.jump = 0
+<<<<<<< Updated upstream
             
             # increase score
             if self.level.obstacles:
                 self.score += self.combo *(config.SCORE_POINTS_PER_SECOND/config.BASE_FPS)
             
             # show particles when the player is in the air
+=======
+
+            # show particles when the player is in the air
+            if self.level.obstacles:
+                self.score += self.combo * (config.SCORE_POINTS_PER_SECOND/config.BASE_FPS)
+>>>>>>> Stashed changes
             self.particle_counter += 1
-            if self.particle_counter>=config.BASE_FPS/config.PARTICLES_PER_SECOND:
+            if self.particle_counter >= config.BASE_FPS/config.PARTICLES_PER_SECOND:
                 self.particles.append(self.rect.y)
                 self.particle_counter = 0
             if not self.vel_y:
