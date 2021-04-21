a474877475b8a7c837363cbcb971556d41eeda77
Sogolumbo <33571916+Sogolumbo@users.noreply.github.com>
2021-04-21 02:50:22 +0200

    Imporve beat finder
    Fix bug found with the song '30 Grad' by MC Fitti.

--- bpm_finder.py
+++ bpm_finder.py
@@ -48,7 +48,7 @@ def findBPMinRange(corrected_autocorrelation, minBPM, maxBPM, duration, fineAdju
     
     interestingPart = corrected_autocorrelation[firstIndex:lastIndex]
     n = 1   # uneven integer, not choosing one leads to worse results
-    ddinterestingPart = np.concatenate([np.zeros(n),np.diff(np.diff(interestingPart, n), n), np.zeros(n)])/(n**2)
+    ddinterestingPart = np.concatenate([np.zeros(n),np.diff(np.diff(interestingPart, n), n), np.zeros(n)])/(n**2) # dd is the second derivative
 
     indexBeatLength = argmax(interestingPart) + firstOffset
     indexBeatLength_dd = argmax(-ddinterestingPart) + firstOffset
@@ -76,10 +76,15 @@ def findBPMinRange(corrected_autocorrelation, minBPM, maxBPM, duration, fineAdju
         plt.show()
 
     if not temposAreSimilar(indexBeatLength, indexBeatLength_dd):
-        # for a valid tempo half the tempo will also have a good autocorrelation
-        score = corrected_autocorrelation[length//2 + 2*indexBeatLength]
-        score_dd = corrected_autocorrelation[length//2 + 2*indexBeatLength_dd]                
-        if score > score_dd:
+        # Compare the quality of the findings by comparing the autocorrelation for 2, 3 and 4 beats
+        beats = np.array([2, 3, 4])
+        scores = np.zeros(len(beats))
+        scores_dd = np.zeros(len(beats))
+        for i, n in enumerate(beats):
+            scores[i] = corrected_autocorrelation[length//2 + n*indexBeatLength]
+            scores_dd[i] = corrected_autocorrelation[length//2 + n*indexBeatLength_dd]  
+         
+        if sum(scores) > sum(scores_dd):
             rough_BPM = BPM
         else:
             rough_BPM = BPM_dd
@@ -93,8 +98,8 @@ def findBPMinRange(corrected_autocorrelation, minBPM, maxBPM, duration, fineAdju
             plt.plot(xRange, corrected_autocorrelation[length//2: length//2 + lastOffset*2])
             plt.scatter(indexBeatLength, interestingPart[indexBeatLength - firstOffset], label=str(BPM) + ' bpm')
             plt.scatter(indexBeatLength_dd, -ddinterestingPart[indexBeatLength_dd - firstOffset], label=str(BPM_dd) + ' bpm')
-            plt.scatter(2*indexBeatLength, score, label=str(0.5*BPM) + ' bpm')
-            plt.scatter(2*indexBeatLength_dd, score_dd, label=str(0.5*BPM_dd) + ' bpm')
+            plt.scatter(beats*indexBeatLength, scores, label=str(BPM) + ' bpm beats')
+            plt.scatter(beats*indexBeatLength_dd, scores_dd, label=str(BPM_dd) + ' bpm beats')
             plt.legend()
             plt.show()
     else:
