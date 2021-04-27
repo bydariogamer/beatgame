cab4e418657bb737f2226de05ee7ee3937f1577f
bydariogamer <62303655+bydariogamer@users.noreply.github.com>
2021-04-27 13:44:21 +0200

    added a audio file transform function

--- utils/sound_transform.py
+++ utils/sound_transform.py
@@ -1,8 +1,11 @@
 import os
+from pydub import AudioSegment
 
 
-def to_ogg(path, replace=False):
-    audio = 
-
-def to_wav(path, replace=False):
-    return
+def transform(path, new_format, replace=False):
+    audio = AudioSegment.from_file(path)
+    name = os.path.basename(path).split('.')[0] + '.' + new_format
+    new_path = os.path.join(os.path.dirname(path), name)
+    audio.export(new_path, new_format)
+    if replace:
+        os.remove(path)
